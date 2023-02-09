7db9dcef984ffb781e8fa326cdb67acb82d739a4
Muhammed Rashid <rashidktr07@gmail.com>
2023-02-09 11:57:02 +0530

    Structure the folder

---
+++ src/NetflixPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContentPage from './Pages/ContentPages/ContentPage'
+import UserPage from './Pages/UserPages/UserPage'
+
+function NetflixPage() {
+  return (
+    <div>
+      <UserPage></UserPage>
+      <ContentPage></ContentPage>
+    </div>
+  )
+}
+
+export default NetflixPage
